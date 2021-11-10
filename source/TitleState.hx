edbb33f488684a86fa9f80fdcca3968443ac9791
CrusherMods <90648119+CrusherMods@users.noreply.github.com>
2021-11-10 12:56:53 -0500

    Switch this to our version

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -80,7 +80,7 @@ class TitleState extends MusicBeatState
 			http.onData = function (data:String)
 			{
 				updateVersion = data.split('\n')[0].trim();
-				var curVersion:String = MainMenuState.psychEngineVersion.trim();
+				var curVersion:String = MainMenuState.guest666Version.trim();
 				trace('version online: ' + updateVersion + ', your version: ' + curVersion);
 				if(updateVersion != curVersion) {
 					trace('versions arent matching!');
